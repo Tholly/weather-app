50a508bd0d4880b1595616c5fe14320157bc4c9e
Tholakele P. Mahlangu <mahlangutholakele60@gmail.com>
2023-07-15 15:19:21 +0200

    Updated weather api
    display humidity , description , weather icon

--- index.js
+++ index.js
@@ -14,24 +14,55 @@ let minutes = (today.getMinutes() < 10 ? "0" : "") + today.getMinutes();
 let dayTime = document.querySelector("#currentdate");
 let currentday = `${days[day]} ${hour}:${minutes}`;
 dayTime.innerHTML = currentday;
-
-function displayTown(event) {
-  event.preventDefault();
-  let input = document.querySelector("#town");
-  input.innerHTML = input.value;
-  let cityweather = document.querySelector("#city");
-  cityweather.innerHTML = input.value;
-  let apiKey = "17ad6e67aa629189f73b053634668b20";
-  let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=${apiKey}`;
-  axios.get(urlApi).then(showTemperature);
-}
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", displayTown);
-
+let celseuisTemperature = null;
 function showTemperature(response) {
+  celseuisTemperature = Math.round(response.data.main.temp);
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = Math.round(response.data.main.temp);
   let humidity = document.querySelector("#humidity");
   let humid = response.data.main.humidity;
   humidity.innerHTML = `Humidity : ${humid}`;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let icon = response.data.weather[0].icon;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${icon}@2x.png`
+  );
 }
+function search(city) {
+  let Apikey = "17ad6e67aa629189f73b053634668b20";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Apikey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#town");
+  search(cityInput.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+function displayFahreinheit(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  let fareinheit = Math.round((celseuisTemperature * 9) / 5 + 32);
+  temperature.innerHTML = fareinheit;
+}
+
+let fahreinheiLink = document.querySelector("#fahreinheit");
+fahreinheiLink.addEventListener("click", displayFahreinheit);
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = celseuisTemperature;
+}
+
+let celsuisLink = document.querySelector("#celsius");
+celsuisLink.addEventListener("click", displayCelsius);
